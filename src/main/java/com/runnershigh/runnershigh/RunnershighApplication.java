c4a41cdc11a0dd229d4fec13e8cd4ad686fe5800
WonYoung <error071312@gmail.com>
2025-09-08 20:09:32 +0900

    init: first commit

---
+++ src/main/java/com/runnershigh/runnershigh/RunnershighApplication.java
@@ -0,0 +1,13 @@
+package com.runnershigh.runnershigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnershighApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnershighApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runnershigh/runnershigh/RunnershighApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runnershigh.runnershigh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnershighApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
