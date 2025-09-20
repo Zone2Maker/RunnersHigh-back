7541e9b0017316245f733a0df7861fe2a6b0a749
earlyerin <rin050301@gmail.com>
2025-09-20 20:27:21 +0900

    feat: crewId 추가 (크루 입장, 생성 메세지)

--- src/main/java/com/runnershigh/runnershigh/dto/message/GetMessageRespDto.java
+++ src/main/java/com/runnershigh/runnershigh/dto/message/GetMessageRespDto.java
@@ -3,11 +3,13 @@ package com.runnershigh.runnershigh.dto.message;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class GetMessageRespDto {
     // Message 필드
@@ -20,4 +22,7 @@ public class GetMessageRespDto {
     // User 필드
     private String nickname;
     private String profileImgUrl;
+
+    // addCrew, joinCrew 시 발행할 crew
+    private Integer crewId;
 }
