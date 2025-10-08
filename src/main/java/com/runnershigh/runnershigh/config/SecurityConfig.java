4611c2b2b2c2baa74393fb1e0a270069c5a472e4
WonYoung <error071312@gmail.com>
2025-10-08 21:35:29 +0900

    fix: securityFilter addFilterBefore 수정

--- src/main/java/com/runnershigh/runnershigh/config/SecurityConfig.java
+++ src/main/java/com/runnershigh/runnershigh/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -53,7 +54,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(Customizer.withDefaults());
+//        http.cors(Customizer.withDefaults());
         http.csrf(csrf -> csrf.disable());
 
         http.formLogin(formLogin -> formLogin.disable());
@@ -62,6 +63,7 @@ public class SecurityConfig {
 
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        http.addFilterBefore(new CorsFilter(corsConfigurationSource()), UsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         http.authorizeHttpRequests(auth -> {
